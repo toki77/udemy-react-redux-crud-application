ccc9cf453f437df71fe0b292cc036e1b0e144f49
時安涼 <ryotokiyasu@tokiyasuryounoMacBook.local>
2021-01-20 01:04:59 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const App = () => {
+const App = () => (<Counter></Counter>)
 
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "No name",age:3 },
-  ]
+class Counter extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {count:0}
+  }
 
-  return (
-    <div>
-      {
-        profiles.map((profile,index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
+  handlePlusButton = () => {
+    this.setState({count : this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({count : this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <>
+        <div>count : {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </>
+    )
+  }
 }
-
-const User = (props) => {
-  return (<div>Hi,I am {props.name} and {props.age} years old!</div>)
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age:PropTypes.number.isRequired
-} 
 
 export default App;
